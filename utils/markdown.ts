7a5610b3a537aa35aa61110683b38c158ce09a5f
조용원 <auau3@kakao.com>
2025-01-15 10:07:55 +0900

    toc 추출 함수 분리

--- utils/markdown.ts
+++ utils/markdown.ts
@@ -6,6 +6,7 @@ import rehypeSanitize from "rehype-sanitize";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
 import rehypeSlug from "rehype-slug";
+import { useCallback } from "react";
 
 export async function markdownToHTML(content: string) {
   const processedContent = await unified()
@@ -19,4 +20,32 @@ export async function markdownToHTML(content: string) {
     .process(content);
 
   return processedContent.toString();
+}
+
+export function getHeadingsFromMdx(mdx: string) {
+  const _getHeadingLevel = (s: string) => s.match(/^(#{1,3})(?=\s)/)![0].length;
+  const _getSanitizedId = (s: string) =>
+    s
+      .match(/^#{1,3}\s+(\S.*)$/)![1]
+      .trimEnd()
+      .replaceAll(" ", "-")
+      .replaceAll("#", "")
+      .toLowerCase();
+
+  const _getHeadings = (mdx: string) => {
+    const lines = mdx.split("\n");
+    return lines
+      .filter((line) => line.startsWith("#"))
+      .map((heading) => {
+        const level = _getHeadingLevel(heading);
+        const id = _getSanitizedId(heading);
+        return {
+          id: id,
+          level: level,
+          content: heading.replaceAll("#", "").trim(),
+        };
+      });
+  };
+
+  return _getHeadings(mdx);
 }

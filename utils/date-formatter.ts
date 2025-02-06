dde7dc502450bc70a107df0e9e3ea21779821d61
조용원 <auau3@kakao.com>
2025-02-06 15:02:57 +0900

    date format 수정

--- utils/date-formatter.ts
+++ utils/date-formatter.ts
@@ -1,4 +1,4 @@
 export function formatDate(date: string | Date) {
   const dateObject = new Date(date);
-  return `${dateObject.getFullYear()}년 ${dateObject.getMonth() < 10 ? `0${dateObject.getMonth() + 1}` : dateObject.getMonth() + 1}월 ${dateObject.getDate()}일`;
+  return `${dateObject.getFullYear()}년 ${dateObject.getMonth() + 1}월  ${dateObject.getDate()}일`;
 }
